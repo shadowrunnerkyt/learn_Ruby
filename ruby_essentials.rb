c83540bd0d940369bf80955298b6d9b3d7c1536f
shadowrunnerkyt <shadowrunnerkyt@users.noreply.github.com>
2019-07-01 22:00:19 +0000

    quoting and interpolation

--- ruby_essentials.rb
+++ ruby_essentials.rb
@@ -4,3 +4,13 @@ puts "Hello World!"
 print "With print, no paren\n"
 print("Print with paren\n")
 
+myname="Chris"
+#print "My name is #{myname}\n"
+printf "My name is %s\n", myname
+
+# Python quotes
+# print("Her pet's name is Kenzy")
+
+puts 'Her pet\'s name is Kenzy'
+puts %q{Her pet's name is Kenzy and she loves "Drawing"}
+
